f6c7bd33c03d756b4394a8b04318650ff22ff032
WLMOSV <yhlyhl119025@gmail.com>
2023-11-21 22:56:27 +0800

    删除了一些文件 差点让slaconf覆盖了conf.h

--- config.h
+++ config.h
@@ -124,4 +124,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
